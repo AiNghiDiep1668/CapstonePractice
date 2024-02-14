f1d95a1d0e6097c5415ae14ba4f0b05dd5c8d85a
Ai Nghi Diep <s_ainghi.diep@ousd.org>
2024-02-14 09:56:05 -0800

    Made some changes

--- app/utils/secrets copy.py
+++ app/utils/secrets copy.py
@@ -2,10 +2,10 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':"",
-        'MONGO_DB_NAME':"",
-        'GOOGLE_CLIENT_ID': "",
-        'GOOGLE_CLIENT_SECRET':"",
+        'MONGO_HOST':"mongodb+srv://AiNghiDiep:Huy0908221100:@ainghicluster0.uxfpiek.mongodb.net/AiNghiPractice?retryWrites=true&w=majority",
+        'MONGO_DB_NAME':"AiNghiPractice",
+        'GOOGLE_CLIENT_ID': "263182430729-d2ql2f4qtsti8cn9av5b2i5r7q2mthh3.apps.googleusercontent.com",
+        'GOOGLE_CLIENT_SECRET':"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",
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
         'MY_EMAIL_ADDRESS':""
         }
